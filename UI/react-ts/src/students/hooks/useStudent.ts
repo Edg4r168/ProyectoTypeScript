b811b1823abf4fcdf99211c04d0714268021eb09
edgar sibrian <edg4r168@gmail.com>
2024-08-26 08:25:06 -0600

    Use useContext

--- UI/react-ts/src/students/hooks/useStudent.ts
+++ UI/react-ts/src/students/hooks/useStudent.ts
@@ -1,76 +1,12 @@
-import { useState } from "react";
-
-import { getAllService } from "../services/getAll";
-import { NewStudent, Student } from "../../types/types";
-import { getByIdService } from "../services/getById";
-import { saveService } from "../services/save";
-import { updateService } from "../services/update";
-import { deleteService } from "../services/delete";
+import { useContext } from "react";
+import { StudentContext } from "../contex/StudentContex";
 
 export function useStudent() {
-  const [error, setError] = useState<string | null>(null);
-  const [students, setStudents] = useState<Student[]>([]);
-  const [student, setStudent] = useState<Student | null>(null);
+  const contex = useContext(StudentContext);
 
-  const getStudents = (page?: number, size?: number) => {
-    getAllService(page, size)
-      .then((res) => setStudents(res))
-      .catch((error: Error) => setError(error.message));
-  };
+  if (contex === undefined) {
+    throw new Error("useStudent must bi used whithin a StudentProvider");
+  }
 
-  const getStudent = (id: number) => {
-    getByIdService(id)
-      .then((res) => setStudent(res))
-      .catch((error: Error) => setError(error.message));
-  };
-
-  const saveStudent = (student: NewStudent) => {
-    saveService(student)
-      .then(({ result: savedStudent }) => {
-        setStudents((prevStudents) => [...prevStudents, savedStudent]);
-      })
-      .catch((error: Error) => setError(error.message));
-  };
-
-  const updateStudent = (student: Student) => {
-    updateService(student)
-      .then(({ result: updatedStudent }) => {
-        const studentIndex = students.findIndex((s) => s.id === student.id);
-
-        const newStudents = [
-          ...students.slice(0, studentIndex),
-          updatedStudent,
-          ...students.slice(studentIndex + 1),
-        ];
-
-        setStudents(newStudents);
-      })
-      .catch((error: Error) => setError(error.message));
-  };
-
-  const removeStudent = (id: number) => {
-    deleteService(id)
-      .then((_res) => {
-        const newStudents = students.filter((student) => student.id !== id);
-
-        setStudents(newStudents);
-      })
-      .catch((error: Error) => setError(error.message));
-  };
-
-  const cleanStudent = () => {
-    setStudent(null);
-  };
-
-  return {
-    error,
-    getStudents,
-    students,
-    getStudent,
-    student,
-    saveStudent,
-    updateStudent,
-    removeStudent,
-    cleanStudent,
-  };
+  return contex;
 }
