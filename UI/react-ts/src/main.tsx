f7615f1990d8230e1c25d4448f086f4a53176810
edgar sibrian <edg4r168@gmail.com>
2024-08-26 08:22:50 -0600

    Add provider

--- UI/react-ts/src/main.tsx
+++ UI/react-ts/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { StudentProvider } from "./students/contex/StudentProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StudentProvider>
+      <App />
+    </StudentProvider>
+  </StrictMode>
+);
